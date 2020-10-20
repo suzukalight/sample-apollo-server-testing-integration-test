9db53e3043edcdc2bc837c037a357d92071c3518
suzukalight <mail@mkubara.com>
2020-10-21 00:52:41 +0900

    Add signInEmailPassword mutation resolver

---
+++ src/infrastructure/apollo-server/resolvers/mutation/Auth/index.ts
@@ -0,0 +1,7 @@
+import { MutationResolvers } from '../../../../../schema/types';
+import { ApolloServerContext } from '../../../types';
+import { signInEmailPassword } from './signInEmailPassword';
+
+export const Auth: MutationResolvers<ApolloServerContext> = {
+  ...signInEmailPassword,
+};
--- src/infrastructure/apollo-server/resolvers/mutation/index.ts
+++ src/infrastructure/apollo-server/resolvers/mutation/index.ts
@@ -1,6 +1,8 @@
 import { MutationResolvers } from '../../../../schema/types';
 import { User } from './User';
+import { Auth } from './Auth';
 
 export const Mutation: MutationResolvers = {
   ...User,
+  ...Auth,
 };
