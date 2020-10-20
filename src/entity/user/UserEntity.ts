828d770c23b65a9ecf020a369e3a61b9f0fdfde2
suzukalight <mail@mkubara.com>
2020-10-20 14:04:36 +0900

    Add user entity and dto

---
+++ src/entity/user/UserEntity.ts
@@ -0,0 +1,79 @@
+import { ValidationError, ConflictError } from '../../common/error';
+import { UserDto, denyIllegalUserDto } from './UserDto';
+import { ID } from '../common/ID';
+import { Email } from '../common/Email';
+import { Role, RoleTypes, RoleType } from '../common/Role';
+
+export class UserEntity {
+  private id: ID;
+  private email: Email;
+  private roles: Role[] = [new Role(RoleTypes.Anonymous)];
+
+  constructor(user: UserDto) {
+    denyIllegalUserDto(user);
+    this.id = new ID(user.id);
+    this.email = new Email(user.email);
+    this.roles = user.roles.map((role) => new Role(role as RoleType));
+  }
+
+  getId() {
+    return this.id;
+  }
+
+  getEmail() {
+    return this.email;
+  }
+
+  getRoles() {
+    return this.roles;
+  }
+
+  setId(id: ID) {
+    this.id = id;
+    this.isValid();
+  }
+
+  setEmail(email: Email) {
+    this.email = email;
+    this.isValid();
+  }
+
+  updateRoles(newRoles: Role[]) {
+    this.roles = newRoles;
+    this.isValid();
+  }
+
+  addRole(targetRole: Role) {
+    if (this.roles.some((role) => role.isEqual(targetRole))) {
+      throw new ConflictError('そのロールはすでに保持しています');
+    }
+
+    this.updateRoles([...(this.roles || []), targetRole]);
+  }
+
+  removeRole(targetRole: Role) {
+    const newRoles = this.roles?.filter((role) => role.isEqual(targetRole));
+    if (!newRoles.length) {
+      throw new ValidationError('すべてのロールを削除することになる操作はできません');
+    }
+
+    this.updateRoles(newRoles);
+  }
+
+  toDto(): UserDto {
+    return {
+      id: this.id.toString(),
+      email: this.email.toString(),
+      roles: this.roles.map((role) => role.toString()),
+    };
+  }
+
+  isValid(): boolean {
+    try {
+      denyIllegalUserDto(this.toDto());
+    } catch {
+      return false;
+    }
+    return true;
+  }
+}
---
+++ src/entity/user/__tests__/index.ts
@@ -0,0 +1,34 @@
+import { PropertyRequiredError, IllegalArgumentError } from '../../../common/error';
+
+import { UserDto } from '../UserDto';
+import { UserEntity } from '../UserEntity';
+import { RoleTypes } from '../../common/Role';
+
+describe('UserEntity', () => {
+  test('エンティティを生成できた', () => {
+    const user = { id: '1', email: 'aaa@bbb.com', roles: [RoleTypes.Member] };
+    const user1 = new UserEntity(user);
+    expect(user1.getId().toString()).toBe(user.id);
+    expect(user1.getEmail().toString()).toBe(user.email);
+  });
+
+  test('idが不足しているため、失敗した', () => {
+    const user = { email: 'aaa@bbb.com', roles: [RoleTypes.Member] };
+    expect(() => new UserEntity((user as unknown) as UserDto)).toThrow(PropertyRequiredError);
+  });
+
+  test('emailが不足しているため、失敗した', () => {
+    const user = { id: '1', roles: [RoleTypes.Member] };
+    expect(() => new UserEntity((user as unknown) as UserDto)).toThrow(PropertyRequiredError);
+  });
+
+  test('rolesが不足しているため、失敗した', () => {
+    const user = { id: '1', email: 'aaa@bbb.com' };
+    expect(() => new UserEntity((user as unknown) as UserDto)).toThrow(PropertyRequiredError);
+  });
+
+  test('不正なメールアドレスを指定したため、失敗した', () => {
+    const user = { id: '1', email: 'aaaaaaaa', roles: [RoleTypes.Member] };
+    expect(() => new UserEntity((user as unknown) as UserDto)).toThrow(IllegalArgumentError);
+  });
+});
